c6de1c830042b8c02db9fcefb9406649b1fe62e9
Ignacio Serrano <ignic@ignic.com>
2024-08-25 13:22:45 +0200

    feat(scripts): remove fastfetch fix (was fixed upstream)

--- config/scripts/customize.sh
+++ config/scripts/customize.sh
@@ -9,8 +9,3 @@ set -oue pipefail
 mkdir -p /usr/share/monokle
 curl -L https://github.com/kubeshop/monokle/releases/download/downloads/Monokle-linux-x86_64.AppImage -o /usr/share/monokle/monokle
 chmod 0755 /usr/share/monokle/monokle
-
-# Fix fastfetch config for some terminals (e.g. kitty)
-mv /usr/share/ublue-os/ublue-os.jsonc /tmp/ublue-os.jsonc
-jq '.display.color.keys = "blue" | .modules[0].color.user = "blue"' /tmp/ublue-os.jsonc > /usr/share/ublue-os/ublue-os.jsonc
-rm -f /tmp/ublue-os.jsonc
